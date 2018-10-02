a0802d36452a18f2df2533fbe761a1a485b1511b
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-02 21:24:32 +0100

    Added gnuke alias

--- config/git/aliases.zsh
+++ config/git/aliases.zsh
@@ -2,6 +2,7 @@ alias g='git'
 alias gp='git push'
 alias gdt='git difftool'
 alias gia='git add'
+alias gnuke='git checkout . && git clean -f'
 alias branch='git branch -a | awk '\''{n = split($1, a, "/"); print a[n]}'\'' | sort -u | fzf'
 alias vbranch='g branch -av | awk '\''{n = split($1, a, "/")} !seen[a[n]]++ { print; }'\'' | fzf | awk '\''{n = split($1, a, "/"); print a[n]}'\'
 alias git=hub
