aeed48bf38d7aeb8f6b2839db0ac0a25f33d6907
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-02 19:42:04 +0100

    Added git add alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,5 +1,6 @@
 alias g='git'
 alias gdt='git difftool'
+alias gia='git add'
 alias branch='git branch -a | awk '\''{n = split($1, a, "/"); print a[n]}'\'' | sort -u | fzf'
 alias vbranch='g branch -av | awk '\''{n = split($1, a, "/")} !seen[a[n]]++ { print; }'\'' | fzf | awk '\''{n = split($1, a, "/"); print a[n]}'\'
 alias git=hub
