34bb4adb1622823dd381880255e4e62997095409
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-02 21:38:13 +0100

    Added tipz bundle

--- config/zshell/bundles.zsh
+++ config/zshell/bundles.zsh
@@ -1,6 +1,9 @@
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions
+antigen bundle aws/aws-cli --loc=bin/aws_zsh_completer.sh
+antigen bundle molovo/tipz
+
 
 antigen apply
 
