5c01cbda98bab1d602f3cf78679597ee5e15a4fa
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-04 15:05:24 +0100

    Added function to print jira board

--- config/jira/helper-functions.zsh
+++ config/jira/helper-functions.zsh
@@ -4,3 +4,16 @@ jira_transition_ticket_fzf() {
   jira transition --noedit "$state" "$ticket"
 }
 
+print_jira_board() {
+  local first_ticket=$(jira sprint | head -n 1 | cut -d ":" -f 1)
+  local transitions=($(jira transitions $first_ticket | cut -d ":" -f 2 | awk '{$1=$1};1' | sed 's/ /_/g'))
+
+  for transition in "${transitions[@]}"; do
+    transition=$(echo $transition | sed 's/_/ /g')
+    echo "\n -----------------------------"
+    echo "       $transition"
+    echo " -----------------------------"
+    jira list --query="project = \"ORBITEN\" AND status = \"$transition\" AND Sprint in openSprints() AND sprint not in futureSprints()"
+  done
+ }
+
