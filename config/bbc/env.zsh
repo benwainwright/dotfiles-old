85e1f60926c08bfa3951ac48ebb0b0ac0cb2bfd1
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-04 22:52:24 +0100

    Moved the locations of the Jenkins URL

--- config/bbc/env.zsh
+++ config/bbc/env.zsh
@@ -17,6 +17,6 @@ export DEV_CERT_LOCATION=/etc/pki/tls/certs/client.crt
 export DEV_KEY_LOCATION=/etc/pki/tls/private/client.key
 export DEV_PEM_LOCATION=/etc/pki/tls/cert.pem
 export DEV_P12_LOCATION=$HOME/workspace/dev.bbc.co.uk.p12
+
+
 export JENKINS_URL=https://jenkins.cd.test.tools.bbc.co.uk
-
-
