b14281efae919213a52a8b06cb3a6f7f543087c4
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-04 22:52:40 +0100

    Added env vars for cosmos CLI

--- config/bbc/env.zsh
+++ config/bbc/env.zsh
@@ -18,5 +18,8 @@ export DEV_KEY_LOCATION=/etc/pki/tls/private/client.key
 export DEV_PEM_LOCATION=/etc/pki/tls/cert.pem
 export DEV_P12_LOCATION=$HOME/workspace/dev.bbc.co.uk.p12
 
+# Needed for cosmos CLI
+export COSMOS_CERT=/etc/pki/tls/cert.pem
+export COSMOS_CERT_KEY=/etc/pki/tls/private/client.key
 
 export JENKINS_URL=https://jenkins.cd.test.tools.bbc.co.uk
