06827296a1025ed3a77ca23d11cb6fa674be557f
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-04 13:39:37 +0100

    Wrap aws with a function that checks for the default value first

--- config/aws/helper-functions.zsh
+++ config/aws/helper-functions.zsh
@@ -1,3 +1,11 @@
 aws_cli_profile_set() {
   export AWS_DEFAULT_PROFILE=$1
 }
+
+aws() {
+  if [[ -z "$AWS_DEFAULT_PROFILE" ]]; then
+    echo "\$AWS_DEFAULT_PROFILE is not set. Run aws_cli_profile_set (profile-name)"
+  else
+    aws "$@"
+  fi
+}
