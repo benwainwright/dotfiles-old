ab07ee0c9e883620852f0825bb11e624ee3f1af6
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-04 23:00:04 +0100

    Added echo to cosmos ssh

--- config/bbc/helper-functions.zsh
+++ config/bbc/helper-functions.zsh
@@ -1,6 +1,7 @@
 ssh-cosmos() {
   local component="$1"
   local environment="$2"
+  echo "Getting cosmos login for '$component' on '$environment'..."
   local ip=$(cosmos login "$component" "$environment" | tail -n 1 | rev | cut -d " " -f 1 | rev)
   ssh $ip,eu-west-1
 }
