efc58d43186f7116237f112f467a4cc6dd309467
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-04 22:53:07 +0100

    Added two new Jenkins SDK methods

--- config/jenkins/helper-functions.zsh
+++ config/jenkins/helper-functions.zsh
@@ -28,13 +28,28 @@ jenkins_request() {
     return
   fi
 
-  curl -X "$method" "$JENKINS_URL/$api_request" \
-      --cert "$DEV_P12_LOCATION:$DEV_P12_PASSWORD" \
-      --key "$DEV_KEY_LOCATION" \
-      --user "$JENKINS_USER:$JENKINS_TOKEN"
+  curl -g -s -X "$method" "$JENKINS_URL/$api_request" \
+       --cert "$DEV_P12_LOCATION:$DEV_P12_PASSWORD" \
+       --key "$DEV_KEY_LOCATION" \
+       --user "$JENKINS_USER:$JENKINS_TOKEN"
 }
 
 build_jenkins_job() {
   local job_name="$1"
   jenkins_request POST job/$job_name/build
+}
+
+get_jenkins_jobs() {
+  local request
+  if [[ -z "$1" ]]; then
+    request="api/json"
+  else
+    request="job/$1/api/json"
+  fi
+  jenkins_request GET "$request" | jq -r '.jobs[].name'
+}
+
+get_jenkins_job_build_history() {
+  request="job/${${1}//\///job/}/api/json?tree=builds[displayName,timestamp,result]"
+  jenkins_request GET "$request" | jq -r '.builds[] | [.displayName, (.timestamp |.  / 1000 | strftime("%l:%M%p %Y-%m-%d")), .result ] | @csv' | tr -d '"' | tr ',' ' '
 }
