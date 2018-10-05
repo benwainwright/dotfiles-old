635623dc3955bb4a18f5ff85b643fd68eef31e0c
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-05 13:01:54 +0100

    Added git pull alias

--- config/git/aliases.zsh
+++ config/git/aliases.zsh
@@ -11,5 +11,7 @@ alias gnuke='git checkout . && git clean -f'
 alias gcps='commit-push'
 alias branch='git branch -a | awk '\''{n = split($1, a, "/"); print a[n]}'\'' | sort -u | fzf'
 alias vbranch='g branch -av | awk '\''{n = split($1, a, "/")} !seen[a[n]]++ { print; }'\'' | fzf | awk '\''{n = split($1, a, "/"); print a[n]}'\'
-alias git=hub
+alias git='hub'
+alias gfm='git pull'
+
 
