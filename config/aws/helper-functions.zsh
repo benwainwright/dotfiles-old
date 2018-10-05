279c7eb24ace81143c94f86c82712f51352caa47
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-05 14:05:18 +0100

    Added helper functions that make ecr-logins easier

--- config/aws/helper-functions.zsh
+++ config/aws/helper-functions.zsh
@@ -1,3 +1,20 @@
 aws_cli_profile_set() {
   export AWS_DEFAULT_PROFILE=$1
 }
+
+ecr-switch-profile-fzf() {
+  local profiles=$(cat ~/.aws/config | grep profile | sed 's/\[profile \(.*\)\]/\1/g')
+  export AWS_DEFAULT_PROFILE=$(echo $profiles | fzf)
+}
+
+ecr-login() {
+  if [ -z $AWS_DEFAULT_PROFILE ] || [ "$1" = "--switch" ]; then
+    ecr-switch-profile-fzf
+  fi
+  $(aws ecr get-login --no-include-email)
+}
+
+ecr-login-with-profile() {
+  export AWS_DEFAULT_PROFILE="$1"
+  $(aws ecr get-login --no-include-email)
+}
