2bc285383737aeae3c1f038e2adc64425f21d742
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-06 23:04:41 +0100

    Split out some of the longer commands from git remove repo if inactive into functions

--- config/git/helper-functions.zsh
+++ config/git/helper-functions.zsh
@@ -12,25 +12,47 @@ git-remove-inactive-repos() {
   local repos=($(find "$1" -type d -mindepth 1 -maxdepth 1))
   local timestamp status
   for repo in "${repos[@]}"; do
-    remove-repo-if-inactive "$repo"
+    git-remove-repo-if-inactive "$repo"
   done
 }
 
-remove-repo-if-inactive() {
+git-has-changes() {
+  local repo="$1"
+  local git_status=$(git \
+    --git-dir="$repo/.git" \
+    --work-tree="$repo" \
+    status \
+    --short)
+  return $([ ! -z "$git_status" ])
+}
+
+git-last-commit-timestamp() {
+  local repo="$1"
+  git \
+    --git-dir "$repo/.git" \
+    show \
+    --pretty=format:%ct \
+    --quiet
+}
+
+git-remove-repo-if-inactive() {
   local one_month_ago=$(gdate +%s -d "1 month ago")
-  if [ -d "$repo/.git" ]; then
-    timestamp=$(git --git-dir "$repo/.git" show --pretty=format:%ct --quiet)
-    if [ $timestamp -lt $one_month_ago ]; then 
-      git_status=$(git --git-dir="$repo/.git" --work-tree="$repo" status --short)
-      if [ ! -z "$git_status" ]; then
-        read "?$repo has uncommitted changes; do you still want to remove it (y/n)? " choice
-        case "$choice" in 
-          n|N ) return;;
-          * ) echo "invalid";;
-        esac
-      fi
-      echo "Removing $repo"
-      rm -rf "$repo"
+  if [ -d "$repo/.git" ] && [ $(git-last-commit-timestamp "$repo") -lt $one_month_ago ]; then
+    if git-has-changes "$repo"; then
+      read "?$repo has uncommitted changes; do you still want to remove it (y/n)? " choice
+      case "$choice" in 
+        n|N ) return;;
+        * ) echo "invalid";;
+      esac
     fi
+    remove-folder "$repo"
   fi
+}
+
+remove-folder() {
+  local folder="$1"
+  printf "Removing %s\n" "$repo"
+  local parent=$(dirname $PWD)
+  rm -rf "$repo"
+  cd "$parent"
 }
