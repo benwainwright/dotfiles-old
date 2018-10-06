a87922d6e58bb896fb790d0c97f43e9055641f60
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-06 17:03:34 +0100

    Added a helper function that prunes old repos from my bbc workspace

--- config/git/helper-functions.zsh
+++ config/git/helper-functions.zsh
@@ -7,3 +7,32 @@ commit-push() {
   fi
   git commit $message && git push
 }
+
+# Removes folders from my BBC workspace where the most recent commit
+# was more than a month ago
+remove-inactive-repos() {
+  local repos=($(find $BBC_WORKSPACE -type d -mindepth 1 -maxdepth 1))
+  local timestamp status
+  for repo in "${repos[@]}"; do
+    remove-repo-if-inactive "$repo"
+  done
+}
+
+remove-repo-if-inactive() {
+  local one_month_ago=$(gdate +%s -d "1 month ago")
+  if [ -d "$repo/.git" ]; then
+    timestamp=$(git --git-dir "$repo/.git" show --pretty=format:%ct --quiet)
+    if [ $timestamp -lt $one_month_ago ]; then 
+      git_status=$(git --git-dir="$repo/.git" --work-tree="$repo" status --short)
+      if [ ! -z "$git_status" ]; then
+        read "?$repo has uncommitted changes; do you still want to remove it (y/n)? " choice
+        case "$choice" in 
+          n|N ) return;;
+          * ) echo "invalid";;
+        esac
+      fi
+      echo "Removing $repo"
+      rm -rf "$repo"
+    fi
+  fi
+}
