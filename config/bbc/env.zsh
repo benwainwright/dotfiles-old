6fb9644a688466faa2a5539fb46cd940e977ec72
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-06 11:34:32 +0100

    Switched 'on network' script

--- config/bbc/env.zsh
+++ config/bbc/env.zsh
@@ -1,17 +1,17 @@
 function is_internal_network {
-    [ "BBC On Network" = "$(/usr/sbin/scselect 2>&1 | grep '^ \*' | sed 's/.*(\(.*\))/\1/')" ]
+    [ "BBC On Network" = "$(networksetup -getcurrentlocation)" ]
 }
  
 # Export the BBC proxy for curl if we're on BBC internal network
-if is_internal_network; then
-    echo "BBC corporate network detected; configuring proxy..."
-    export http_proxy="http://www-cache.reith.bbc.co.uk:80"
-    export https_proxy="http://www-cache.reith.bbc.co.uk:80"
-    export HTTPS_PROXY="http://www-cache.reith.bbc.co.uk:80"
-    export FTP_PROXY="ftp-gw.reith.bbc.co.uk:21"
-    export ftp_proxy="ftp-gw.reith.bbc.co.uk:21"
-    export ALL_PROXY="$http_proxy"
-fi
+# if is_internal_network; then
+#     echo "BBC corporate network detected; configuring proxy..."
+#     export http_proxy="http://www-cache.reith.bbc.co.uk:80"
+#     export https_proxy="http://www-cache.reith.bbc.co.uk:80"
+#     export HTTPS_PROXY="http://www-cache.reith.bbc.co.uk:80"
+#     export FTP_PROXY="ftp-gw.reith.bbc.co.uk:21"
+#     export ftp_proxy="ftp-gw.reith.bbc.co.uk:21"
+#     export ALL_PROXY="$http_proxy"
+# fi
 
 export DEV_CERT_LOCATION=/etc/pki/tls/certs/client.crt
 export DEV_KEY_LOCATION=/etc/pki/tls/private/client.key
