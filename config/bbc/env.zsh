7176f08127be8625720c30ad92830ed84eab6be1
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-06 17:01:58 +0100

    Turn if_is_internal_network back on

--- config/bbc/env.zsh
+++ config/bbc/env.zsh
@@ -3,15 +3,15 @@ function is_internal_network {
 }
  
 # Export the BBC proxy for curl if we're on BBC internal network
-# if is_internal_network; then
-#     echo "BBC corporate network detected; configuring proxy..."
-#     export http_proxy="http://www-cache.reith.bbc.co.uk:80"
-#     export https_proxy="http://www-cache.reith.bbc.co.uk:80"
-#     export HTTPS_PROXY="http://www-cache.reith.bbc.co.uk:80"
-#     export FTP_PROXY="ftp-gw.reith.bbc.co.uk:21"
-#     export ftp_proxy="ftp-gw.reith.bbc.co.uk:21"
-#     export ALL_PROXY="$http_proxy"
-# fi
+if is_internal_network; then
+    echo "BBC corporate network detected; configuring proxy..."
+    export http_proxy="http://www-cache.reith.bbc.co.uk:80"
+    export https_proxy="http://www-cache.reith.bbc.co.uk:80"
+    export HTTPS_PROXY="http://www-cache.reith.bbc.co.uk:80"
+    export FTP_PROXY="ftp-gw.reith.bbc.co.uk:21"
+    export ftp_proxy="ftp-gw.reith.bbc.co.uk:21"
+    export ALL_PROXY="$http_proxy"
+fi
 
 export DEV_CERT_LOCATION=/etc/pki/tls/certs/client.crt
 export DEV_KEY_LOCATION=/etc/pki/tls/private/client.key
