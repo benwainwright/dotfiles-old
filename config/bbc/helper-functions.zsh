d7f155ea8359fab22b0673fa8f73f58e6d876447
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-06 17:03:06 +0100

    Added function that either goes to a folder in the bbc workspace if it exists, or checks it out

--- config/bbc/helper-functions.zsh
+++ config/bbc/helper-functions.zsh
@@ -5,3 +5,13 @@ ssh-cosmos() {
   local ip=$(cosmos login "$component" "$environment" | tail -n 1 | rev | cut -d " " -f 1 | rev)
   ssh $ip,eu-west-1
 }
+
+bbc-repo() {
+  local dir="$BBC_WORKSPACE/$1"
+  if [ ! -d "$dir" ]; then
+    if hub clone "bbc/$1" $dir; then
+      cd "$dir"
+    fi
+  fi
+  cd "$dir"
+}
