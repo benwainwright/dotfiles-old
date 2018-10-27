05dee35d071c0e9d1806e7f9db57229c609504e9
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-10-27 19:41:59 +0100

    Added a bunch of dir aliases

--- config/misc/aliases.sh
+++ config/misc/aliases.sh
@@ -1,5 +1,13 @@
 alias ls='gls --color=always'
-alias ll='ls -laGh'
+alias l='ls -lFh'     #size,show type,human readable
+alias la='ls -lAFh'   #long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
+alias ll='ls -l'      #long list
+alias ldot='ls -ld .*'
+alias lS='ls -1FSsh'
+alias lart='ls -1Fcart'
+alias lrt='ls -1Fcrt'
 alias ..='cd ..'
 alias m='make'
 alias cat='bat'
