5c37eeefee00aecac7e1b10633b5b982678c8ae7
Ben Wainwright <ben.wainwright@bbc.co.uk>
2018-11-07 10:16:11 +0000

    Added function get the pipeline into the shell

--- config/jenkins/helper-functions.zsh
+++ config/jenkins/helper-functions.zsh
@@ -28,10 +28,14 @@ jenkins_request() {
     return
   fi
 
-  curl -g -s -X "$method" "$JENKINS_URL/$api_request" \
-       --cert "$DEV_P12_LOCATION:$DEV_P12_PASSWORD" \
+  curl --fail \
+       --silent \
+       --globoff \
+       --request "$method" \
+       --cert "$DEV_CERT_LOCATION" \
        --key "$DEV_KEY_LOCATION" \
-       --user "$JENKINS_USER:$JENKINS_TOKEN"
+       --user "$JENKINS_USER:$JENKINS_TOKEN" \
+       "$JENKINS_URL/$api_request" \
 }
 
 build_jenkins_job() {
@@ -46,10 +50,32 @@ get_jenkins_jobs() {
   else
     request="job/$1/api/json"
   fi
-  jenkins_request GET "$request" | jq -r '.jobs[].name'
+  if ! jenkins_request GET "$request" | jq -r '.jobs[].name'; then
+    echo "Failed to get Jenkins jobs..."
+  fi
 }
 
 get_jenkins_job_build_history() {
-  request="job/${${1}//\///job/}/api/json?tree=builds[displayName,timestamp,result]"
+  local request="job/${${1}//\///job/}/api/json?tree=builds[displayName,timestamp,result]"
   jenkins_request GET "$request" | jq -r '.builds[] | [.displayName, (.timestamp |.  / 1000 | strftime("%l:%M%p %Y-%m-%d")), .result ] | @csv' | tr -d '"' | tr ',' ' '
+}
+
+get_pipeline_status() {
+  local build_number=$2
+  local request="job/${${1}//\///job/}/$build_number/wfapi/"
+  IFS=$'\n'
+  local output=($(jenkins_request GET "$request" | jq -r '.stages[] | [.status, .name] | @tsv'))
+  for line in "${output[@]}"; do
+    if [[ $line =~ "FAILED" ]]; then
+      printf "\e[31m%s\e[39m\n" "$line"
+    elif [[ $line =~ "SUCCESS" ]]; then
+      printf "\e[32m%s\e[39m\n" "$line"
+    elif [[ $line =~ "PAUSED" ]]; then
+      printf "\e[34m%s\e[39m\n" "$line"
+    elif [[ $line =~ "UNSTABLE" ]]; then
+      printf "\e[33m%s\e[39m\n" "$line"
+    else
+      printf "%s\n" "$line"
+    fi
+  done
 }
