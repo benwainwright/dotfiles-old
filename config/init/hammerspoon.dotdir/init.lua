ee4011b16e66b8f8579fec1b6c2cd4938ac9481e
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-03-08 19:17:36 +0000

    Switched to just command alt for hotkeys

--- config/init/hammerspoon.dotdir/init.lua
+++ config/init/hammerspoon.dotdir/init.lua
@@ -5,12 +5,12 @@ local mash = {"cmd", "alt", "ctrl"}
 local spectacle = {"cmd", "alt"}
 local spectacleshift = {"cmd", "alt", "shift"}
 
-hs.hotkey.bind(mash, 'M', function() hs.application.launchOrFocus('Spotify') end)
-hs.hotkey.bind(mash, 'S', function() hs.application.launchOrFocus('Slack') end)
-hs.hotkey.bind(mash, 'T', function() hs.application.launchOrFocus('Microsoft Outlook') end)
-hs.hotkey.bind(mash, 'I', function() hs.application.launchOrFocus('iTerm') end)
-hs.hotkey.bind(mash, 'B', function() hs.application.launchOrFocus('Safari') end)
-hs.hotkey.bind(mash, 'E', function() hs.application.launchOrFocus('Evernote') end)
-hs.hotkey.bind(mash, 'O', function() hs.application.launchOrFocus('Microsoft Outlook') end)
-hs.hotkey.bind(mash, 'D', function() hs.application.launchOrFocus('Dash') end)
-hs.hotkey.bind(mash, 'F', function() hs.application.launchOrFocus('Finder') end)
+hs.hotkey.bind(spectacle, 'M', function() hs.application.launchOrFocus('Spotify') end)
+hs.hotkey.bind(spectacle, 'S', function() hs.application.launchOrFocus('Slack') end)
+hs.hotkey.bind(spectacle, 'T', function() hs.application.launchOrFocus('Microsoft Outlook') end)
+hs.hotkey.bind(spectacle, 'I', function() hs.application.launchOrFocus('iTerm') end)
+hs.hotkey.bind(spectacle, 'B', function() hs.application.launchOrFocus('Safari') end)
+hs.hotkey.bind(spectacle, 'E', function() hs.application.launchOrFocus('Evernote') end)
+hs.hotkey.bind(spectacle, 'O', function() hs.application.launchOrFocus('Microsoft Outlook') end)
+hs.hotkey.bind(spectacle, 'D', function() hs.application.launchOrFocus('Dash') end)
+hs.hotkey.bind(spectacle, 'F', function() hs.application.launchOrFocus('Finder') end)
