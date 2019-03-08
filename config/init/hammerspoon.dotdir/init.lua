95c52806250f9d41ee11db9cadfc852b1ae33133
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-03-08 21:43:29 +0000

    Added kieran's window movement stuff

--- config/init/hammerspoon.dotdir/init.lua
+++ config/init/hammerspoon.dotdir/init.lua
@@ -1,16 +1,74 @@
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
+local hotkey = hs.hotkey
+local application = hs.application
+local window = hs.window
+local geometry = hs.geometry
+local appfinder = hs.appfinder
+
 local mash = {"cmd", "alt", "ctrl"}
 local spectacle = {"cmd", "alt"}
 local spectacleshift = {"cmd", "alt", "shift"}
 
-hs.hotkey.bind(spectacle, 'M', function() hs.application.launchOrFocus('Spotify') end)
-hs.hotkey.bind(spectacle, 'S', function() hs.application.launchOrFocus('Slack') end)
-hs.hotkey.bind(spectacle, 'T', function() hs.application.launchOrFocus('Microsoft Outlook') end)
-hs.hotkey.bind(spectacle, 'I', function() hs.application.launchOrFocus('iTerm') end)
-hs.hotkey.bind(spectacle, 'B', function() hs.application.launchOrFocus('Safari') end)
-hs.hotkey.bind(spectacle, 'E', function() hs.application.launchOrFocus('Evernote') end)
-hs.hotkey.bind(spectacle, 'O', function() hs.application.launchOrFocus('Microsoft Outlook') end)
-hs.hotkey.bind(spectacle, 'D', function() hs.application.launchOrFocus('Dash') end)
-hs.hotkey.bind(spectacle, 'F', function() hs.application.launchOrFocus('Finder') end)
+hotkey.bind(spectacle, 'M', function() application.launchOrFocus('Spotify') end)
+hotkey.bind(spectacle, 'S', function() application.launchOrFocus('Slack') end)
+hotkey.bind(spectacle, 'T', function() application.launchOrFocus('Microsoft Outlook') end)
+hotkey.bind(spectacle, 'I', function() application.launchOrFocus('iTerm') end)
+hotkey.bind(spectacle, 'B', function() application.launchOrFocus('Safari') end)
+hotkey.bind(spectacle, 'E', function() application.launchOrFocus('Evernote') end)
+hotkey.bind(spectacle, 'O', function() application.launchOrFocus('Microsoft Outlook') end)
+hotkey.bind(spectacle, 'D', function() application.launchOrFocus('Dash') end)
+hotkey.bind(spectacle, 'F', function() application.launchOrFocus('Finder') end)
+
+
+function moveTo(win, x, y, h, w)
+  local rect = geometry.rect(x, y, h, w)
+  win:moveToUnit(rect)
+end
+
+function moveNamedAppTo(name, x, y, h, w)
+    local app = appfinder.appFromName(name)
+    moveTo(app:mainWindow(), x, y, h, w)
+    app:mainWindow():focus()
+end
+
+hotkey.bind(spectacle, "H", function()
+    moveTo(window.focusedWindow(), 0, 0, 0.5, 1)
+end)
+
+hotkey.bind(spectacle, "J", function()
+    moveTo(window.focusedWindow(), 0, 0.5, 1, 0.5)
+end)
+
+hotkey.bind(spectacle, "K", function()
+    moveTo(window.focusedWindow(), 0, 0, 1, 0.5)
+end)
+
+hotkey.bind(spectacle, "L", function()
+    moveTo(window.focusedWindow(), 0.5, 0, 0.5, 1)
+end)
+
+hotkey.bind(spectacleshift, "H", function()
+    moveTo(window.focusedWindow(), 0, 0, 0.5, 1)
+    moveNamedAppTo('iTerm2', 0.5, 0, 0.5, 1)
+end)
+
+hotkey.bind(spectacleshift, "J", function()
+    moveTo(window.focusedWindow(), 0, 0.5, 1, 0.5)
+    moveNamedAppTo('iTerm2', 0, 0, 1, 0.5)
+end)
+
+hotkey.bind(spectacleshift, "K", function()
+    moveTo(window.focusedWindow(), 0, 0, 1, 0.5)
+    moveNamedAppTo('iTerm2', 0, 0.5, 1, 0.5)
+end)
+
+hotkey.bind(spectacleshift, "L", function()
+    moveTo(window.focusedWindow(), 0.5, 0, 0.5, 1)
+    moveNamedAppTo('iTerm2', 0, 0, 0.5, 1)
+end) 
+-- Full screen.
+hotkey.bind(spectacle, "F", function()
+    local win = window.focusedWindow():maximize()
+end)
