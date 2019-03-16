6a604e2ad98025fd5e8650814933978850c03d43
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-03-16 14:24:43 +0000

    Added brew aliases

--- config/brew/aliases.zsh
+++ config/brew/aliases.zsh
@@ -1,1 +1,8 @@
 alias bi='brew install'
+alias  brew_list_formula='find /usr/local/Homebrew/ -type d -name "Formula" -exec ls -1 {} \; | cut -d"." -f1'
+alias blf='brew_list_formula'
+alias brew_list_formula_fzf="brew_list_formula | fzf --height=40% --preview='brew info {}'"
+alias blff='brew_list_formula_fzf'
+alias bif='brew install $(blff)'
+
+
