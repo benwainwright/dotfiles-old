7fbf35c5952a4d0657bdf1236fe834f79f317a77
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-03-21 18:40:42 +0000

    Increased font size

--- config/emacs/emacs.d.dotdir/config/visual.el
+++ config/emacs/emacs.d.dotdir/config/visual.el
@@ -1,7 +1,7 @@
 (setq initial-frame-alist
   '((tool-bar-lines . 0)
      (width . 106)
-     (font . "Fira Code-13")
+     (font . "Fira Code-14")
      (height . 60)
      (background-mode . 'dark)
      (ns-transparent-titlebar . t)
@@ -9,7 +9,7 @@
 
 (setq default-frame-alist
   '((tool-bar-lines . 0)
-     (font . "Fira Code-13")
+     (font . "Fira Code-14")
      (width . 106)
      (height . 60)
      (background-mode . 'dark)
