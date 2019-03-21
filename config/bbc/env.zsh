52676b85778c9591427a26f9c04c5b3056b96847
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-03-21 18:39:07 +0000

    Allow pip to use BBC cloud cert

--- config/bbc/env.zsh
+++ config/bbc/env.zsh
@@ -12,3 +12,10 @@ export JENKINS_URL=https://jenkins.cd.tools.bbc.co.uk
 
 export CHRONOS=kieran-bamforth,andrewscfc,lalkhum,cefn,saralk
 export BBC_WORKSPACE=$HOME/workspace
+
+# This allows PIP to use the BBC cloud cert as well as public CAs
+# Do this with the following set of commands
+# curl --location https://curl.haxx.se/ca/cacert.pem >/tmp/cert.pem
+# curl --location https://curl.haxx.se/ca/cacert.pem >/tmp/cert.pem
+# mv /tmp/cert.pem /usr/local/etc/openssl/cert.pem
+export PIP_CERT=/usr/local/etc/openssl/cert.pem
