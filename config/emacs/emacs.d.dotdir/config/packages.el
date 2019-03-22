8247a1c8fba6f6a1ca88ef4f68f42092904a9215
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-03-22 21:48:37 +0000

    Don't use vc

--- config/emacs/emacs.d.dotdir/config/packages.el
+++ config/emacs/emacs.d.dotdir/config/packages.el
@@ -116,7 +116,9 @@
 (use-package magit
   :ensure t
   :config
-  (remove-hook 'server-switch-hook 'magit-commit-diff))
+  (remove-hook 'server-switch-hook 'magit-commit-diff)
+  (setq vc-handled-backends nil)
+  )
 
 (use-package magit-popup
   :ensure t
