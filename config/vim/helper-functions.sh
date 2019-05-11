f517acfaf91e9f1ec2769022815a4c478a07b93b
Ben Wainwright <ben.wainwright@bbc.co.uk>
2019-05-11 22:58:15 +0100

    Split commnads into two invocations

--- config/vim/helper-functions.sh
+++ config/vim/helper-functions.sh
@@ -2,7 +2,8 @@
 gvim() {
   if [ "$#" -eq 0 ]; then
     if [ "$(command gvim --serverlist)" ]; then
-      command gvim --remote-expr "foreground() || execute('cd $PWD')" > /dev/null
+      command gvim --remote-expr "execute('cd $PWD')" > /dev/null
+      command gvim --remote-expr "foreground()" > /dev/null
     else
       command gvim
     fi
