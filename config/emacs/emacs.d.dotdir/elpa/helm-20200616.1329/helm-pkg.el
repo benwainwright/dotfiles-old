26405f0e5a7478e5dde0c4fe055cbb8208c7aa18
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-17 21:36:09 +0100

    Remove my committed elpa directory

--- config/emacs/emacs.d.dotdir/elpa/helm-20200616.1329/helm-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "helm" "20200616.1329" "Helm is an Emacs incremental and narrowing framework"
-  '((emacs "25.1")
-    (async "1.9.4")
-    (popup "0.5.3")
-    (helm-core "3.6.2"))
-  :commit "a9bc75235f51687772e6f9f6e61c5c4f849d1aaf" :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- config/emacs/emacs.d.dotdir/elpa/helm-core-20200605.647/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20200605.647" "Development files for Helm"
-  '((emacs "25.1")
-    (async "1.9.4"))
-  :commit "a9bc75235f51687772e6f9f6e61c5c4f849d1aaf" :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
