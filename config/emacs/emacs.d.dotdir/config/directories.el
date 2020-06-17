6bfce25319457e60ee1bfd9eed2c64d54e93ce6f
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-17 23:01:28 +0100

    Don't litter my working directory with hash files

--- config/emacs/emacs.d.dotdir/config/directories.el
+++ config/emacs/emacs.d.dotdir/config/directories.el
@@ -1,3 +1,17 @@
 (provide 'directories)
 
 (setq custom-file (concat user-emacs-directory "/custom.el"))
+
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+
+(make-directory user-temporary-file-directory t)
+
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+	(,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
