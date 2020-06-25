cf3b8c584ffa107f86b4b2b3cfd18992d1d44e25
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-25 16:30:45 +0100

    Add org-agenda files

--- config/emacs/emacs.d.dotdir/config/directories.el
+++ config/emacs/emacs.d.dotdir/config/directories.el
@@ -3,3 +3,7 @@
 (setq custom-file (concat user-emacs-directory "/custom.el"))
 
 (setq org-directory "~/Google Drive/org/")
+
+(setq org-agenda-files '("~/Google Drive/org/personal.org"
+                         "~/Google Drive/org/work.org"
+                         "~/Google Drive/org/inbox.org"))
