16e6cbf80b21bbf28e03608ddbd6864ca861251b
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-06-26 09:23:30 +0100

    Add doom stuff

--- config/emacs/emacs.d.dotdir/config/packages.el
+++ config/emacs/emacs.d.dotdir/config/packages.el
@@ -17,21 +17,36 @@
 ;;; Use-package definitions ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
+
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
 (use-package exec-path-from-shell
   :ensure t
   :config
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
-
-;; (use-package telephone-line
-;;   :ensure t
-;;   :config
-;;   (telephone-line-mode 1))
-
-(use-package moe-theme
-  :ensure t
-  :config
-  (load-theme 'moe-dark t))
 
 ;; Vim emulation
 (use-package evil
