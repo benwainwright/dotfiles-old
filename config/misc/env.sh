4d2a28ec75c400a4633d747f86f80d829e144366
Ben Wainwright <ben.wainwright@bbc.co.uk>
2020-09-20 10:17:43 +0100

    Stuff

--- config/misc/env.sh
+++ config/misc/env.sh
@@ -1,4 +1,4 @@
-export FZF_DEFAULT_COMMAND='rg --files --hidden'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --preview-window=right:hidden'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS='--cycle --extended'
 export PROJECT_HOME='$HOME/workspace'
