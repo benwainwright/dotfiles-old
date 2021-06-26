fdf0d94913fc73dad8bddad65292680ebe6eaf94
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-26 15:56:36 +0100

    Stuff

--- config/nvm/nvm.sh
+++ config/nvm/nvm.sh
@@ -3,5 +3,5 @@ nvm() {
     export NVM_DIR="$HOME/.nvm"
     [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
     [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-    nvm $@
+    command nvm $@
 }
