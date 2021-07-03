9e2d25fa77949c7cefc96dd38ba4d6e6d054288f
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-07-03 23:45:52 +0100

    Improved install automatiion

--- config/nvm/nvm.sh
+++ config/nvm/nvm.sh
@@ -1,7 +1,1 @@
 
-nvm() {
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-    [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-    command nvm $@
-}
