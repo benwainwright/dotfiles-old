814e0f46897d2ae0984d1fa732e641bc0d941afe
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-07-16 01:36:33 +0100

    Moving more stuff over to lua

--- config/xdg/config.dotdir/nvim/lua/plugins.lua
+++ config/xdg/config.dotdir/nvim/lua/plugins.lua
@@ -1,12 +1,117 @@
 return require('packer').startup(function(use)
 
+  use 'mhinz/vim-startify'
+  use 'nvim-lua/plenary.nvim'
+  use 'kshenoy/vim-signature'
+
   use {
-    "knubie/vim-kitty-navigator"
+    "p00f/nvim-ts-rainbow",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        rainbow = {
+          enable = true,
+          extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+          max_file_lines = 1000 -- Do not enable for files with more than 1000 lines, int
+        }
+      }
+    end
+  }
+
+
+
+  use {
+    'lepture/vim-velocity',
+    ft = "velocity"
+  }
+
+  use 'mattn/emmet-vim'
+
+  use 'mfussenegger/nvim-dap'
+  use {
+    'tweekmonster/startuptime.vim',
+    command = "StartupTime"
+  }
+
+  use 'honza/vim-snippets'
+  use 'mlaursen/vim-react-snippets'
+
+  use 'wellle/targets.vim'
+  use 'gioele/vim-autoswap'
+  use 'ryanoasis/vim-devicons'
+  use 'chrisbra/unicode.vim'
+  use 'sindrets/diffview.nvim'
+
+  use {
+    'jiangmiao/auto-pairs',
+    event = "InsertEnter",
+  }
+
+  use 'bkad/CamelCaseMotion'
+  use 'PeterRincker/vim-argumentative'
+  use 'michaeljsmith/vim-indent-object'
+  use 'vim-test/vim-test'
+  use {
+    'kyazdani42/nvim-tree.lua',
+    cmd = "NvimTreeToggle",
+    config = function()
+      vim.api.nvim_set_var("nvim_tree_highlight_opened_files", 1)
+      vim.api.nvim_set_var("nvim_tree_git_hl", 1)
+      vim.api.nvim_set_var("nvim_tree_lsp_diagnostics", 1)
+      vim.api.nvim_set_var("nvim_tree_update_cwd", 1)
+      vim.api.nvim_set_var("nvim_tree_follow", 1)
+    end
   }
 
   use {
-    "fladson/vim-kitty"
+    'hrsh7th/nvim-compe',
+    event = "InsertEnter",
+    config = function() require("load-compe") end
   }
+
+  use {
+    'tzachar/compe-tabnine',
+    run = "./install.sh",
+    requires = "hrsh7th/nvim-compe",
+    event = "InsertEnter"
+  }
+
+  use "glepnir/lspsaga.nvim"
+  use 'kosayoda/nvim-lightbulb'
+  use 'RishabhRD/nvim-lsputils'
+  use 'kabouzeid/nvim-lspinstall'
+  use 'neovim/nvim-lspconfig'
+
+  use {
+    'jose-elias-alvarez/nvim-lsp-ts-utils',
+    {
+      requires = {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function() require("null-ls").setup() end
+      }
+    }
+  }
+
+  use 'kyazdani42/nvim-web-devicons'
+  use 'RishabhRD/popfix'
+  use 'onsails/lspkind-nvim'
+  use 'ynkdir/vim-vimlparser'
+  use 'jparise/vim-graphql'
+  use { 
+    "sbdchd/neoformat",
+    cmd = "Neoformat"
+  }
+
+  use "knubie/vim-kitty-navigator"
+  use "junegunn/fzf.vim"
+  use "junegunn/fzf"
+  use "mfussenegger/nvim-dap"
+  use 'tpope/vim-rhubarb'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-dispatch'
+  use 'tpope/vim-eunuch'
+  use 'tpope/vim-commentary'
+  use "fladson/vim-kitty"
 
   use {
     "SirVer/ultisnips",
@@ -18,6 +123,7 @@ return require('packer').startup(function(use)
   use {
     "folke/trouble.nvim",
     requires = "kyazdani42/nvim-web-devicons",
+    cmd = { "Trouble", "LspTrouble" },
     config = function()
       require("trouble").setup()
     end
@@ -25,14 +131,26 @@ return require('packer').startup(function(use)
 
   use { 
     "weirongxu/plantuml-previewer.vim",
+    ft = "plantuml",
     requires = {
       "tyru/open-browser.vim",
       "aklt/plantuml-syntax"
     }
   }
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'benwainwright/fzf-project' }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+      local ts = require 'nvim-treesitter.configs'
+      ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
+    end
+  }
+
+  use {
+    'benwainwright/fzf-project',
+    cmd = { "FzfSwitchProject", "FzfChooseProjectFile" }
+  }
 
   use { 
     'projekt0n/github-nvim-theme',
@@ -46,6 +164,7 @@ return require('packer').startup(function(use)
 
   use {
     'TimUntersberger/neogit',
+    cmd = "Neogit",
     config = function() require("neogit").setup() end
   }
 
@@ -67,20 +186,5 @@ return require('packer').startup(function(use)
     end
   }
 
-  use {
-    'neovim/nvim-lspconfig',
-  }
-  use {
-    'jose-elias-alvarez/nvim-lsp-ts-utils',
-    {
-      requires = {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function() require("null-ls").setup() end
-      }
-    }
-  }
 
-  use {
-    "glepnir/lspsaga.nvim"
-  }
 end)
