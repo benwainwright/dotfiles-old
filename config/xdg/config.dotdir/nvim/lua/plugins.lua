96d67fbf4187dd83d7d977855ddcf6867c56e51c
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-09-25 11:57:28 +0100

    stuff

--- config/xdg/config.dotdir/nvim/lua/plugins.lua
+++ config/xdg/config.dotdir/nvim/lua/plugins.lua
@@ -1,4 +1,4 @@
-return require('packer').startup(function(use)
+return require('packer').startup({ function(use)
 
   use {
     'David-Kunz/jester'
@@ -51,48 +51,6 @@ return require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  -- use {
-  --   'akinsho/nvim-toggleterm.lua',
-  --   config = function()
-      
-  --     require("toggleterm").setup {
-  --       -- size can be a number or function which is passed the current terminal
-  --       size = function(term)
-  --         if term.direction == "horizontal" then
-  --           return 15
-  --         elseif term.direction == "vertical" then
-  --           return vim.o.columns * 0.4
-  --         end
-  --       end,
-
-  --       open_mapping = [[<c-\>]],
-  --       hide_numbers = true, -- hide the number column in toggleterm buffers
-  --       shade_filetypes = {},
-  --       shade_terminals = true,
-  --       shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-  --       start_in_insert = true,
-  --       insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  --       persist_size = true,
-  --       direction = 'horizontal',
-  --       close_on_exit = true, -- close the terminal window when the process exits
-  --       shell = vim.o.shell, -- change the default shell
-  --       -- This field is only relevant if direction is set to 'float'
-  --       float_opts = {
-  --         -- The border key is *almost* the same as 'nvim_win_open'
-  --         -- see :h nvim_win_open for details on borders however
-  --         -- the 'curved' border is a custom border type
-  --         -- not natively supported but implemented in this plugin.
-  --         border = 'single',
-  --         winblend = 3,
-  --         highlights = {
-  --           border = "Normal",
-  --           background = "Normal",
-  --         }
-  --       }
-  --     }
-  --   end
-  -- }
-
   use {
     'glepnir/dashboard-nvim',
     requires="liuchengxu/vim-clap"
@@ -107,7 +65,6 @@ return require('packer').startup(function(use)
   -- Add extra text objects
   use 'wellle/targets.vim'
 
-  -- Rainbow parens
   use {
     "p00f/nvim-ts-rainbow",
     config = function()
@@ -176,8 +133,11 @@ return require('packer').startup(function(use)
     command = "StartupTime"
   }
 
+  use {
+    'honza/vim-snippets',
+    rtp = '.'
+  }
 
-  use 'honza/vim-snippets'
   use 'mlaursen/vim-react-snippets'
 
   -- Behave sensibly with swap files
@@ -240,6 +200,12 @@ return require('packer').startup(function(use)
 
   use {
     'hrsh7th/nvim-cmp',
+    requires = {
+      'quangnguyen30192/cmp-nvim-ultisnips',
+      { 'tzachar/cmp-tabnine', run='./install.sh' },
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp'
+    },
     config = function()
       local lspkind = require'lspkind'
 
@@ -290,13 +256,13 @@ return require('packer').startup(function(use)
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.close(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
         },
         sources = {
-          { name = 'cmp_tabnine'},
           { name = 'nvim_lsp' },
-          { name = 'ultisnips' },
+          { name = 'cmp_tabnine'},
           { name = 'buffer' },
+          { name = 'ultisnips' }
         },
         documentation = {
           border = {
@@ -305,24 +271,6 @@ return require('packer').startup(function(use)
         }
       })
     end
-  }
-
- 	use {
-    'tzachar/cmp-tabnine',
-    run='./install.sh',
-    requires = 'hrsh7th/nvim-cmp'
-  }
-
-  use {
-    'hrsh7th/cmp-nvim-lsp'
-  }
-
-  use {
-    'hrsh7th/cmp-buffer'
-  }
-
-  use {
-    'quangnguyen30192/cmp-nvim-ultisnips'
   }
 
   use "glepnir/lspsaga.nvim"
@@ -345,7 +293,6 @@ return require('packer').startup(function(use)
   }
 
   use 'kyazdani42/nvim-web-devicons'
-  use 'RishabhRD/popfix'
   use 'onsails/lspkind-nvim'
   use 'ynkdir/vim-vimlparser'
   use "knubie/vim-kitty-navigator"
@@ -390,7 +337,15 @@ return require('packer').startup(function(use)
     run = ':TSUpdate',
     config = function()
       local ts = require 'nvim-treesitter.configs'
-      ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
+      ts.setup {
+        ensure_installed = 'maintained',
+        highlight = {
+          enable = true
+        },
+        indent = {
+          enable = true
+        }
+      }
     end
   }
 
@@ -484,4 +439,4 @@ return require('packer').startup(function(use)
     end
   }
 
-end)
+end, config = { max_jobs = 50 }})
