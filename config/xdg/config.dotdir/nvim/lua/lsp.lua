fcb25e09b9d1bc17df2c91573f95405df2517f08
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-12-18 11:12:06 +0000

    setup lua language server

--- config/xdg/config.dotdir/nvim/lua/lsp.lua
+++ config/xdg/config.dotdir/nvim/lua/lsp.lua
@@ -1,11 +1,7 @@
-local lspConfig = require('lspconfig')
--- local lspInstall = require('lspinstall')
-
 local on_attach = function(client, bufnr)
 
   require "lsp_signature".on_attach()  -- Note: add in lsp client on-attach
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   local opts = { noremap=true, silent=true }
 
@@ -71,37 +67,11 @@ local on_attach = function(client, bufnr)
 
 end
 
---local lspInstallServers = {
---  'bash',
---  'vim',
---  'graphql',
---  'typescript',
---  'diagnosticls',
---  'dockerfile',
---  'go',
---  'yaml',
---  'python',
---  'css',
---  'json',
---  --'lua'
---}
-
---for _, server in ipairs(lspInstallServers) do
---  if not lspInstall.is_server_installed(server) then
---    lspInstall.install_server(server)
---  end
---end
-
--- lspInstall.setup()
-
-local lsp_installer = require("nvim-lsp-installer")
-
 local lsp_installer_servers = require'nvim-lsp-installer.servers'
 
 local dls_server_available, dls_requested_server = lsp_installer_servers.get_server("diagnosticls")
 if dls_server_available then
     dls_requested_server:on_ready(function ()
-        local opts = {}
         dls_requested_server:setup {
           on_attach = on_attach,
           filetypes = {"typescript", "typescriptreact"},
@@ -203,7 +173,6 @@ end
 local ts_server_available, ts_requested_server = lsp_installer_servers.get_server("tsserver")
 if ts_server_available then
     ts_requested_server:on_ready(function ()
-        local opts = {}
         ts_requested_server:setup {
           debounce_text_changes = 150,
           on_attach = function(client, bufnr)
@@ -219,8 +188,6 @@ if ts_server_available then
               watch_dir = nil,
             }
 
-
-
             client.resolved_capabilities.document_formatting = false
             on_attach(client, bufnr)
             ts_utils.setup_client(client)
@@ -228,136 +195,25 @@ if ts_server_available then
         }
     end)
     if not ts_requested_server:is_installed() then
-        -- Queue the server to be installed
         ts_requested_server:install()
     end
 end
 
--- local servers = lspInstall.installed_servers()
+local lua_server_available, lua_requested_server = lsp_installer_servers.get_server("sumneko_lua")
+if lua_server_available then
+    lua_requested_server:on_ready(function ()
+        lua_requested_server:setup {
+          settings = {
+            Lua = {
+              diagnostics = {
+                  globals = {'vim'},
+              },
+            }
+          }
 
--- for _, server in pairs(servers) do
---   if server == "typescript" then
-
---     lspConfig.typescript.setup {
---       debounce_text_changes = 150,
---       on_attach = function(client, bufnr)
-
---         local ts_utils = require("nvim-lsp-ts-utils")
-
---         ts_utils.setup {
---           eslint_enable_diagnostics = false,
---           disable_commands = false,
---           enable_formatting = false,
---           update_imports_on_move = true,
---           require_confirmation_on_move = false,
---           watch_dir = nil,
---         }
-
-
-
---         client.resolved_capabilities.document_formatting = false
---         on_attach(client, bufnr)
---         ts_utils.setup_client(client)
---       end
---     }
-
---   elseif server == "diagnosticls" then
-
---     lspConfig.diagnosticls.setup {
---       on_attach = on_attach,
---       filetypes = {"typescript", "typescriptreact"},
---       flags = {
---         debounce_text_changes = 150,
---       },
---       init_options = {
---         linters = {
---           eslint = {
---             command = 'eslint_d',
---             rootPatterns = { '.eslintrc.js', '.eslintrc.json', 'package.json' },
---             debounce = 100,
---             args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
---             sourceName = 'eslint',
---             parseJson = {
---               errorsRoot = '[0].messages',
---               line = 'line',
---               column = 'column',
---               endLine = 'endLine',
---               endColumn = 'endColumn',
---               message = '[eslint] ${message} [${ruleId}]',
---               security = 'severity'
---             },
---             securities = {
---               [2] = 'error',
---               [1] = 'warning'
---             }
---           },
---           markdownlint = {
---             command = 'markdownlint',
---             rootPatterns = { '.git' },
---             isStderr = true,
---             debounce = 100,
---             args = { '--stdin' },
---             offsetLine = 0,
---             offsetColumn = 0,
---             sourceName = 'markdownlint',
---             securities = {
---               undefined = 'hint'
---             },
---             formatLines = 1,
---             formatPattern = {
---               '^.*:(\\d+)\\s+(.*)$',
---               {
---                 line = 1,
---                 column = -1,
---                 message = 2,
---               }
---             }
---           }
---         },
---         filetypes = {
---           javascript = 'eslint',
---           javascriptreact = 'eslint',
---           typescript = 'eslint',
---           typescriptreact = 'eslint',
---           markdown = 'markdownlint',
---           pandoc = 'markdownlint'
---         },
---         formatters = {
---           prettier = {
---             args = { '--stdin', '--stdin-filepath', '%filepath' },
-
---             rootPatterns = {
---               '.prettierrc',
---               '.prettierrc.json',
---               '.prettierrc.toml',
---               '.prettierrc.json',
---               '.prettierrc.yml',
---               '.prettierrc.yaml',
---               '.prettierrc.json5',
---               '.prettierrc.js',
---               '.prettierrc.cjs',
---               'prettier.config.js',
---               'prettier.config.cjs',
---             },
---             command = 'prettier_d_slim'
---           }
---         },
---         formatFiletypes = {
---           css = 'prettier',
---           javascript = 'prettier',
---           javascriptreact = 'prettier',
---           json = 'prettier',
---           scss = 'prettier',
---           typescript = 'prettier',
---           typescriptreact = 'prettier'
---         }
---       }
---     }
---   else
---     lspConfig[server].setup {
---       capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
---       on_attach = on_attach,
---     }
---   end
--- end
-
+        }
+    end)
+    if not lua_requested_server:is_installed() then
+        lua_requested_server:install()
+    end
+end
