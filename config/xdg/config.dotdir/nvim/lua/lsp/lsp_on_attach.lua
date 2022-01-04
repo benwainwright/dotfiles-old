937e654e4f9aaeb6b83abf9f4aaaa2393d060dd6
Ben Wainwright <ben.wainwright@cinch.co.uk>
2022-01-04 16:07:45 +0000

    Virtual text

--- config/xdg/config.dotdir/nvim/lua/lsp/lsp_on_attach.lua
+++ config/xdg/config.dotdir/nvim/lua/lsp/lsp_on_attach.lua
@@ -45,7 +45,7 @@ function M.global_lsp_config(client, bufnr)
 
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = false,
+      virtual_text = true,
       underline = true,
       signs = true,
     }
