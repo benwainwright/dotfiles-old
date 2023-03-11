f5780f90a4eb413f112265eb53574acdb09c7a4a
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-03-11 18:41:48 +0000

    stuff

--- install/scripts/install-linux-deps.sh
+++ install/scripts/install-linux-deps.sh
@@ -1,5 +1,5 @@
 install_linux_deps() {
   sudo curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
-
+  curl https://pyenv.run | bash
   wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 }
--- install/scripts/symlink-dotfiles.sh
+++ install/scripts/symlink-dotfiles.sh
@@ -3,7 +3,7 @@ symlink_dotfiles() {
   setopt extended_glob
   setopt NULL_GLOB
 
-  printf "\n\nSymlinking dotfiles\n***\n\n"
+  printf "\n\n** Symlinking dotfiles ***\n\n"
 
   readonly dots=($DOTFILES/config/**/*.{dotfile,dotdir})
   for dot in "${dots[@]/.\//}"; do
@@ -12,7 +12,7 @@ symlink_dotfiles() {
   done
 
   if [[ $(uname) == "Darwin" ]]; then
-    printf "\n\nSymlinking mac specific dotfiles\n***\n\n"
+    printf "\n\n***Symlinking mac specific dotfiles ***\n\n"
     readonly dots=($DOTFILES/config/**/*.{dotfile.mac,dotdir.mac})
     for dot in "${dots[@]/.\//}"; do
       link_name=."$(echo "$dot" | xargs basename -s .dotfile | xargs basename -s .dotdir)"
@@ -21,7 +21,7 @@ symlink_dotfiles() {
   fi
 
   if command -v apt > /dev/null || command -v freebsd-version > /dev/null; then
-    printf "\n\nSymlinking linux specific dotfiles\n***\n\n"
+    printf "\n\n*** Symlinking linux specific dotfiles ***\n\n"
     readonly dots=($DOTFILES/config/**/*.{dotfile.linux,dotdir.linux})
     for dot in "${dots[@]/.\//}"; do
       link_name=."$(echo "$dot" | xargs basename -s .dotfile | xargs basename -s .dotdir)"
@@ -30,7 +30,7 @@ symlink_dotfiles() {
   fi
 
   if command -v apt > /dev/null; then
-    printf "\n\nSymlinking debian specific dotfiles\n***\n\n"
+    printf "\n\n*** Symlinking debian specific dotfiles ***\n\n"
     readonly dots=($DOTFILES/config/**/*.{dotfile.deb,dotdir.deb})
     for dot in "${dots[@]/.\//}"; do
       link_name=."$(echo "$dot" | xargs basename -s .dotfile | xargs basename -s .dotdir)"
@@ -39,7 +39,7 @@ symlink_dotfiles() {
   fi
 
   if command -v freebsd-version > /dev/null; then
-    printf "\nSymlinking freebsd specific dotfiles"
+    printf "\n\n*** Symlinking freebsd specific dotfiles ***"
     readonly dots=($DOTFILES/config/**/*.{dotfile.freebsd,dotdir.freebsd})
     for dot in "${dots[@]/.\//}"; do
       link_name=."$(echo "$dot" | xargs basename -s .dotfile | xargs basename -s .dotdir)"
