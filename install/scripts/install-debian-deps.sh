e99ccf9bf3024ddad09ad3d16bd63e78eacc9775
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-03-11 18:57:33 +0000

    stuff

--- install/scripts/install-debian-deps.sh
+++ install/scripts/install-debian-deps.sh
@@ -15,4 +15,16 @@ install_debian_deps() {
     yarn \
     
   sudo chsh -s "$(which zsh)"
+
+  local KEY_NAME=id_25519.github
+
+  ssh-keygen -t ed25519 -C "bwainwright28@gmail.com" -f ~/.ssh/$KEY_NAME -q -N ""
+
+  printf "\n%s\n\n" "Copy and paste the following SSH public key into the github interface. Press enter when this is complete."
+  cat ~/.ssh/$KEY_NAME.pub
+  read ans
+
+  cat ~/.ssh/$KEY_NAME.pub
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/$KEY_NAME
 }
