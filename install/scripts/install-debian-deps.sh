2ab38b0537b7f07e423d8f74e7e12602fabd5a67
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-03-11 17:32:02 +0000

    stuff

--- install/scripts/install-debian-deps.sh
+++ install/scripts/install-debian-deps.sh
@@ -1,9 +1,21 @@
 install_debian_deps() {
   sudo apt-get update
 
-  sudo apt-get install \
+  sudo apt-get install -y \
     vim \
-    zsh
-
+    zsh \
+    exa \
+    bat \
+    fasd \
+    fzf \
+    htop \
+    jq \
+    nvm \
+    pyenv \
+    ripgrep \
+    thefuck \
+    silversearcher-ag \
+    yarn \
+    
   sudo chsh -s "$(which zsh)"
 }
