d125d2ca0a3759bf8fc63e312e57a3039b954297
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-03-11 19:26:27 +0000

    stuff

--- install/scripts/generate-ssh.sh
+++ install/scripts/generate-ssh.sh
@@ -1,5 +1,5 @@
 generate_ssh() {
-  local KEY_NAME=id_25519.github
+  local KEY_NAME=id_ed25519.github
   ssh-keygen -t ed25519 -C "bwainwright28@gmail.com" -f ~/.ssh/$KEY_NAME -q -N ""
 
   printf "\n%s\n\n" "Copy and paste the following SSH public key into the github interface. Press enter when this is complete."
